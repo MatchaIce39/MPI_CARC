cf8ddb27f02c7e032350b13332c139e3a1958173
MatchaIce39 <ntjmw6657@gmail.com>
2026-06-25 16:17:42 -0600

    Finished the serial version of the computing pi program

--- parallelPi.c
+++ parallelPi.c
@@ -9,30 +9,36 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 
-	int b = 1;
-	int a = 0;
-	int result = 0;
-	int input;
+	double b = 1.0;
+	double a = 0.0;
+	double result;
+	double input;
 	int n = atoi(argv[1]);
 
-	int deltaX = (b - a) / n;
+	//Determine delta X using our integration bounds and number of intervals desired
+	double deltaX = (b - a) / n;
+	printf(" (%f - %f) / %f. The value of delta X is %f\n", b,a,n,deltaX);
 
-	//Inputting a in the function
-	int temp = (4 / (1 + pow(a,2)));
+	//compute the first iteration with the lower integration bound
+	double temp = (4 / (1 + pow(a,2)));
+	printf("value of f(0) is %f\n", temp);
 	result = result + temp;
+	printf("Result is %f\n", result);
 
 	for (int i = 0; i < (n-1); i++){
 		input = input + (a + deltaX);
-		int tempFunction = (4 / (1 + pow(input, 2)));
+		printf("Next interval is %f\n", input);
+		double tempFunction = (4 / (1 + pow(input, 2)));
+		printf("f of %f is %f", input, tempFunction);
 		tempFunction = tempFunction * 2;
-		result = result + tempFunction; 
+		result = result + tempFunction;
+		printf("Total result: %f\n", result);
 	}
 
 	temp = (4 / (1 + pow(b,2)));
 	result = result + temp;
-
 	result = result * (deltaX/2);
 
-	printf("The approximation of the function is %d\n", result);
+	printf("The approximation of the function is %f\n", result);
 	return 0;
 }
