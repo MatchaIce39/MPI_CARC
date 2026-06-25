3ac05e20e244bc6965809f75ea7acef19eaa73d9
MatchaIce39 <nasaamd211@gmail.com>
2026-06-25 14:24:31 -0600

    Fixed the deadlock issue in circularShift.c by rewriting it without any loops

--- circularshift.c
+++ circularshift.c
@@ -4,18 +4,35 @@
 int main(int argc, char** argv) {
 
 	int my_rank, my_size;
-	//int counter = 2;
-	int numberToReceive, numberToSend;
+	int numToSend, numToReceive;
+	int currentProcess = 0;
+	int neighbor = currentProcess + 1;
+
 	MPI_Init(&argc, &argv);
-	//MPI_Status status;
 	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &my_size);
-
-	numberToSend = 67;
-	printf("Sending %d to process 1\n", numberToSend);
 	
-	MPI_Send(&numberToSend, 1, MPI_INT, 1, 10, MPI_COMM_WORLD);
-	MPI_Recv(&numberToReceive, 1, MPI_INT, 0, 10, MPI_COMM_WORLD, NULL);
+	numToSend = 67;
+
+	//printf("Process: %d. Sending to my neighbor: %d", my_rank, (my_rank + 1));
+
+	if(my_rank != (my_size - 1)) {
+
+		if(my_rank == 0) {
+			printf("Process %d sending to Process %d\n", my_rank, (my_rank + 1));
+			MPI_Send(&numToSend, 1, MPI_INT, (my_rank + 1), 10, MPI_COMM_WORLD);
+			printf("Process %d waiting for message from process %d\n", my_rank, (my_size - 1));
+			MPI_Recv(&numToReceive, 1, MPI_INT, (my_size - 1), 11, MPI_COMM_WORLD, NULL);
+		}
+		else{
+			MPI_Recv(&numToReceive, 1, MPI_INT, (my_rank - 1), 10, MPI_COMM_WORLD, NULL);
+			MPI_Send(&numToSend, 1, MPI_INT, (my_rank + 1), 10, MPI_COMM_WORLD);
+		}
+	}
+	else if(my_rank == (my_size - 1)) {
+		printf("Got to the end!\n");
+		MPI_Send(&numToSend, 1, MPI_INT, 0, 11, MPI_COMM_WORLD);
+	}
 
 	MPI_Finalize();
 
