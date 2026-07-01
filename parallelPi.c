b1a43edfcbf1a061896e496b3518db226e4b937e
MatchaIce39 <ntjmw6657@gmail.com>
2026-07-01 13:21:18 -0600

    Finished the parallel pi program, added a bin folder that is hidden for MPI binaries

--- parallelPi.c
+++ parallelPi.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[]) {
 	MPI_Comm_size(MPI_COMM_WORLD, &my_size);
 
 	if(argc < 2) {
-		printf("Missing arguments\n");
+		printf("Missing arguments for number of rectangles\n");
 		return 1;
 	}
 
@@ -24,20 +24,20 @@ int main(int argc, char *argv[]) {
 	double deltaX = 0;
 	int n = atoi(argv[1]);
 
-	printf("Hello\n");
+	//printf("Hello\n");
 
 	if(my_rank == 0) {
 	
 		//Determine delta X using our integration bounds and number of intervals desired
 		deltaX = (b - a) / n;
-		printf(" (%f - %f) / %f. The value of delta X is %f\n", b,a,n,deltaX);
+		//printf(" (%f - %f) / %f. The value of delta X is %f\n", b,a,n,deltaX);
 
 		//compute the first iteration with the lower integration bound
 		temp = (4 / (1 + pow(a,2)));
-		printf("value of f(0) is %f\n", temp);
+		//printf("value of f(0) is %f\n", temp);
 
 		result = result + temp;
-		printf("Result is %f\n", result);
+		//printf("Result is %f\n", result);
 
 		testValue = testValue + 50;
 	}
@@ -46,36 +46,28 @@ int main(int argc, char *argv[]) {
         MPI_Bcast(&result, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
         MPI_Bcast(&testValue, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-	printf("value of test is: %d\n", testValue);
-	printf("value of deltaX is: %f\n", deltaX);
-	printf("value of result is: %f\n", result);
+	//printf("value of test is: %d\n", testValue);
+	//printf("value of deltaX is: %f\n", deltaX);
+	//printf("value of result is: %f\n", result);
 
+	//Line to change
+	input = input + (deltaX * (my_rank + 1));
 
-	//Test
-
-	//Last edge case I will skip for now
-	/*
-	if (my_rank == (my_size - 1)) {
-		//temp = (4 / (1 + pow(b,2)));
-                //result = result + temp;
-                //result = result * (deltaX/2);
-	}
-	*/
-
-        else {
-
-		input = input + (a + deltaX);
-		//printf("Next interval is %f\n", input);
-		double tempFunction = (4 / (1 + pow(input, 2)));
-		//printf("f of %f is %f", input, tempFunction);
-		tempFunction = tempFunction * 2;
-		result = result + tempFunction;
-		//printf("Total result: %f\n", result);
+	double tempFunction = (4 / (1 + pow(input, 2)));
+	tempFunction = tempFunction * 2;
 	
-	}
-
-
+	//MPI_Bcast(&input, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+	MPI_Reduce(&tempFunction, &result, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 	
+	if(my_rank == 0) {
+
+		temp = (4 / (1 + pow(b,2)));
+                result = result + temp;
+                result = result * (deltaX/2);
+	
+		printf("Result is %f with %d processes\n", result, n);
+		printf("The actual value of pi is 3.14159265\n");
+	}
 
 	MPI_Finalize();
 	return 0;
